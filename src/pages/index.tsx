a4a1c3147e7e4f41248b0268b8aeaf0bd5e6f974
Victor Matheus <victormatheus507@gmail.com>
2024-04-06 19:16:28 -0300

    Estrutura de sucesso

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import 'keen-slider/keen-slider.min.css';
 import { stripe } from '@/lib/stripe';
 import { GetStaticProps } from 'next';
 import Stripe from 'stripe';
+import Link from 'next/link';
 
 interface HomeProps {
   products: {
@@ -29,7 +30,8 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-            <Product  href={`/product/${product.id}`} key={product.id} className="keen-slider__slide">
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className="keen-slider__slide">
               <Image src={product.imageUrl} width={520} height={480} alt="" />
 
               <footer>
@@ -37,6 +39,7 @@ export default function Home({ products }: HomeProps) {
                 <span>{product.price}</span>
               </footer>
             </Product>
+          </Link>
         );
       })}
     </HomeContainer>
